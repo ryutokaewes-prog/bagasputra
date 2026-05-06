7b24620323bd03d6f183efeb7bfa458ce49fce08
Putrasal <suaja74@gmail.com>
2026-05-06 12:43:31 +0700

    seller

---
+++ lib/Seller/putra_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PutraHome extends StatefulWidget {
+  const PutraHome({super.key});
+
+  @override
+  State<PutraHome> createState() => _PutraHomeState();
+}
+
+class _PutraHomeState extends State<PutraHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
